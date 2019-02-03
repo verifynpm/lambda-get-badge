b0c58db7bbed0e16950a39ed5eece19b719b8504
Steve Konves <stephen.konves@gmail.com>
2019-02-02 21:47:24 -0700

    feat: fix badge contents

--- src/index.ts
+++ src/index.ts
@@ -11,16 +11,23 @@ export const handler: APIGatewayProxyHandler = async (event, context) => {
 
     const { status } = apiRes.data;
 
-    const color = mapStatusToColor(apiRes.data.status);
+    const color = mapStatusToColor(status);
+    const text = mapStatusToText(status);
 
-    const shieldRes = await axios.get<PackageVersion>(
-      `https://img.shields.io/badge/tbv-${status}-${color}.svg`,
+    const shieldRes = await axios.get(
+      `https://img.shields.io/badge/verification-${text}-${color}.svg`,
     );
 
+    const headers = { 'content-type': 'image/svg+xml' };
+
+    if (status === 'verified') {
+      headers['Cache-Control'] = 'public, max-age=31536000';
+    }
+
     return {
-      headers: { 'content-type': 'image/svg+xml' },
+      headers,
       statusCode: 200,
-      body: JSON.stringify(shieldRes.data),
+      body: shieldRes.data,
     };
   } catch {
     const shieldRes = await axios.get<PackageVersion>(
@@ -30,7 +37,7 @@ export const handler: APIGatewayProxyHandler = async (event, context) => {
     return {
       headers: { 'content-type': 'image/svg+xml' },
       statusCode: 200,
-      body: JSON.stringify(shieldRes.data),
+      body: shieldRes.data,
     };
   }
 };
@@ -49,6 +56,17 @@ function mapStatusToColor(status: PackageVersion['status']): Color {
       return 'yellow';
     case 'verified':
       return 'brightgreen';
+  }
+}
+
+function mapStatusToText(status: PackageVersion['status']): string {
+  switch (status) {
+    case 'unverified':
+      return 'failed';
+    case 'verified':
+      return 'passing';
+    default:
+      return status;
   }
 }
 
