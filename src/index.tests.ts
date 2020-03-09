6a46b9d37d8024bb11bd4e46ea2505e827ea0bb2
Steve Konves <stephen.konves@gmail.com>
2020-03-09 10:27:01 -0700

    tests: add test

--- src/index.tests.ts
+++ src/index.tests.ts
@@ -1,1 +1,21 @@
 import { expect } from 'chai';
+
+import { handler } from '.';
+
+describe('badge', () => {
+  it('works', async () => {
+    // ARRANGE
+    const packageVersion = 'redux@4.0.1';
+
+    //event.pathParameters['packageVersion']
+    const event = {
+      pathParameters: { packageVersion },
+    } as any;
+
+    // ACT
+    const result = await handler(event, null, null);
+
+    // ASSERT
+    console.log(result);
+  });
+});
